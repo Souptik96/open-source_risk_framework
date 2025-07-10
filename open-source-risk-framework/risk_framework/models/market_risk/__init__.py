f2f9b5e85fee112a6af2f81b3786954e130c72d3
Souptik Chakraborty <62941615+Souptik96@users.noreply.github.com>
2025-07-10 18:16:52 +0530

    Update __init__.py

---
+++ open-source-risk-framework/risk_framework/models/market_risk/__init__.py
@@ -0,0 +1,5 @@
+from .var_historical import HistoricalVaR
+from .expected_shortfall import ExpectedShortfall
+from .garch_volatility import GARCHModel
+
+__all__ = ['HistoricalVaR', 'ExpectedShortfall', 'GARCHModel']
